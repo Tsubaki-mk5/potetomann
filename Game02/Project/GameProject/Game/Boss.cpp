34aff3df529aaf713f3248dc69b78c8cf2d47584
Tsubaki-mk5 <mnt525.k@icloud.com>
2022-11-07 11:03:58 +0900

    a

--- Game02/Project/GameProject/Game/Boss.cpp
+++ Game02/Project/GameProject/Game/Boss.cpp
@@ -1,33 +1,21 @@
 #include "Boss.h"
 
-Boss::Boss(const CVector2D& p, bool flip) : Base(eType_Boss) {
-	m_img = COPY_RESOURCE("Boss", CImage);
-	m_img.ChangeAnimation(0);
-	m_pos = p;
-	m_img.SetCenter(480, 480);
-	m_img.SetSize(500, 500);
-	m_rect = CRect(-350, -250, 0, 0);
-	m_rad = 270;
-	m_flip = flip;
-	m_is_ground = false;
-	m_hp = 500;
+Boss::Boss(const CVector2D& pos) : Base(eType_Boss)
+{
+	m_img.Load("Image/Boss.png");
+	m_pos = pos;
+	m_rad = 12;
+	m_img.SetSize(32, 32);
+	m_img.SetCenter(16, 16);
 }
-
 void Boss::Update()
 {
 	
+
 }
 
 void Boss::Draw()
 {
-	m_img.SetPos(GetScreenPos(m_pos));
-	m_img.SetFlipH(m_flip);
+	m_img.SetPos(m_pos);
 	m_img.Draw();
-	DrawRect();
-
-	
-}
-
-void Boss::Collision(Base* b)
-{
 }
