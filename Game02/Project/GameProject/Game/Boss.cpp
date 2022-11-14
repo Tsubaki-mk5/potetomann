a5c2021996967b8b1fb58b72658347df968e8337
Tsubaki-mk5 <mnt525.k@icloud.com>
2022-11-14 11:02:32 +0900

    s

--- Game02/Project/GameProject/Game/Boss.cpp
+++ Game02/Project/GameProject/Game/Boss.cpp
@@ -2,10 +2,10 @@
 
 Boss::Boss(const CVector2D& pos) : Base(eType_Boss)
 {
-	m_img.Load("Image/Boss.png");
+	m_img.Load("Image/sentouki.png");
 	m_pos = pos;
 	m_rad = 12;
-	m_img.SetSize(120, 120);
+	m_img.SetSize(220, 220);
 	m_img.SetCenter(16, 16);
 }
 void Boss::Update()
