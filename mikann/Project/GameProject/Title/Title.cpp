ceb5e8f17ea884f00e869fd66a6372b11b6b9223
kobuside <koun810114@gmail.com>
2022-12-12 11:01:13 +0900

    h

--- mikann/Project/GameProject/Title/Title.cpp
+++ mikann/Project/GameProject/Title/Title.cpp
@@ -1,12 +1,12 @@
 #include "Title.h"
 #include "../Game/Game.h"
+#include "../Game/Game02.h"
 
 
 Title::Title() :Base(eType_Scene), m_title_text("C:\\Windows\\Fonts\\msgothic.ttc", 50), m_menu_text("C:\\Windows\\Fonts\\msgothic.ttc", 50), m_member_text("C:\\Windows\\Fonts\\msgothic.ttc", 40) {
 	m_img = COPY_RESOURCE("Title", CImage);
 	//SOUND("titleBGM")->Play(true);
 }
-
 Title::~Title() {
 	//全てのオブジェクトを破棄
 	Base::KillAll();
@@ -15,7 +15,11 @@ Title::~Title() {
 		//ゲームシーンへ
 		Base::Add(new Game());
 	}
-
+	if (HOLD(CInput::eButton2)) {//Xキー
+		//ゲームシーンへ
+		Base::Add(new Game02());
+	}
+	
 }
 
 void Title::Update() {
